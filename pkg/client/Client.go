e8192e48273c28b57f7ac3932eea1422fc2b4193
Water <water@WaterdeiMac.local>
2020-01-02 18:28:38 +0800

    二十万个为什么

--- pkg/client/Client.go
+++ pkg/client/Client.go
@@ -1,7 +1,9 @@
 package client
 
 import (
-	pb "goodgoodstudy.com/go-grpc/pkg/pb"
+	pb "go-grpc/pkg/pb"
+	"google.golang.org/grpc"
+	"log"
 )
 
 const (
@@ -12,10 +14,22 @@ type ConsumeClient struct {
 }
 
 func NewConsumeClient() (*ConsumeClient, error) {
-	return &ConsumeClient{}, nil // TODO
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		log.Printf("failed to listen: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewControlClient(conn)
+	// 这个client应该在cmd/client调用才是
+	// 怎么让cmd/client调用……
+
+	return &ConsumeClient{}, nil // 这个函数要负责什么做什么 TODO
 }
 
 func (c *ConsumeClient) Pay(req *pb.ConsumeReq) (resp *pb.ConsumeResp, err error) {
 	// TODO
+	// 这里的Pay方法应该写什么
+	// 怎么联系服务器哦
+	// 想有一个葫芦，可以画一下瓢，啊哈哈哈哈哈。
 	return
 }
