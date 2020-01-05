847c538134ccf9b8003dd34c3102080c42206742
Water <water@WaterdeiMac.local>
2020-01-05 23:15:53 +0800

    map is error

--- cmd/Client.go
+++ cmd/Client.go
@@ -3,34 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
-
-	"goodgoodstudy.com/go-grpc/pkg/client"
+	//"goodgoodstudy.com/go-grpc/pkg/client"
 )
 
 func main() {
 
 	// 初始化客户端
-	consumeClient, err := client.NewConsumeClient()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	//consumeClient, err := client.NewConsumeClient()
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//
+	//resp, err := consumeClient.Pay(nil) // TODO
+	//if err != nil {
+	//	log.Println("consume failed,", err)
+	//	return
+	//}
+	//// 怎么把Inp的返回值传给client包哦
+	//// 除此之外，这里还要做什么
 
-	resp, err := consumeClient.Pay(nil) // TODO
-	if err != nil {
-		log.Println("consume failed,", err)
-		return
-	}
-	// 怎么把Inp的返回值传给client包哦
-	// 除此之外，这里还要做什么
+	//log.Println(resp)
 
-	log.Println(resp)
+	fmt.Printf("InputData: %v", Inp())		// InputData: map[itemId:1 itemNum:1 userId:1]
 }
 
- func Inp() (ItemId int64, num int64, UserId int32) {
+ func Inp() map[string]int {
 	var(
-		item int
-		ItemNum int
-		user int
+		item int64
+		ItemNum int32
+		user int64
 	)
 	fmt.Printf("Please intput item_id:")
 	itemId, err := fmt.Scanln(&item)
@@ -49,6 +50,10 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-
-	return int64(itemId), int64(itemNum), int32(userId)
+	var InputData = map[string]int{
+		"itemId":  itemId,
+		"itemNum": itemNum,
+		"userId":  userId,
+	}
+	 return InputData
  }
