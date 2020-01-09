a7afcb75383908456f1935f33780de60caa202a7
Water <water@WaterdeiMac.local>
2020-01-10 00:02:43 +0800

    TODO

--- pkg/client/Client.go
+++ pkg/client/Client.go
@@ -1,9 +1,13 @@
 package client
 
 import (
+	"bufio"
+	"fmt"
 	pb "goodgoodstudy.com/go-grpc/pkg/pb"
 	"google.golang.org/grpc"
 	"log"
+	"os"
+	"strconv"
 )
 
 const (
@@ -20,16 +24,59 @@ func NewConsumeClient() (*ConsumeClient, error) {
 	}
 	defer conn.Close()
 	client := pb.NewControlClient(conn)
-	// 这个client应该在cmd/client调用才是
-	// 怎么让cmd/client调用……
+	client.Pay()	// TODO 这里的pay()的参数呀
 
-	return &ConsumeClient{}, nil // 这个函数要负责什么做什么 TODO
+	return &ConsumeClient{}, nil // TODO
 }
 
+// 获取input数据
 func (c *ConsumeClient) Pay(req *pb.ConsumeReq) (resp *pb.ConsumeResp, err error) {
 	// TODO
-	// 这里的Pay方法应该写什么
-	// 怎么联系服务器哦
-	// 想有一个葫芦，可以画一下瓢，啊哈哈哈哈哈。
-	return
+	InputData := Input()
+	Itemid, _ := strconv.ParseInt(InputData["item_id"], 10 , 64)
+	ItemCount, _ := strconv.ParseInt(InputData["itemCount"], 10, 32)
+	UeserId, _ := strconv.ParseInt(InputData["user_id"], 10, 64)
+
+	return &pb.ConsumeResp{OrderId: resp.GetOrderId(), Message: resp.GetMessage()}, nil
+}
+
+// 读取终端输入
+func Input() map[string]string {
+	var err error
+	input := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("Please enter item id:")
+	item_id, err := input.ReadString('\n')
+	if err != nil {
+		log.Println("input item_id", err)
+	}
+
+	fmt.Printf("Please enter Count:")
+	itemnum, err := input.ReadString('\n')
+	if err != nil {
+		log.Println("input itemCount", err)
+	}
+
+	fmt.Printf("Please enter userId:")
+	userid, err := input.ReadString('\n')
+	if err != nil {
+		log.Println("input userid", err)
+	}
+
+	fmt.Printf("remark or not:")
+	descr, err := input.ReadString('\n')
+	if descr == "" {
+		descr = "nil"
+	}
+	if err != nil {
+		log.Println("description", err)
+	}
+
+	var InputMap = map[string]string{
+		"item_id": item_id,
+		"itemCount": itemnum,
+		"user_id": userid,
+		"descri": descr
+	}
+	return InputMap
 }
