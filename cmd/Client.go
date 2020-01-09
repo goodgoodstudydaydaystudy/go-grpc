cba874fa1b7c0c1bb62bc818efc97ae42b518130
Water <water@WaterdeiMac.local>
2020-01-10 03:17:56 +0800

    add Input()

--- cmd/Client.go
+++ cmd/Client.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"goodgoodstudy.com/go-grpc/pkg/client"
 	"log"
+	"os"
 )
 
 func main() {
@@ -20,4 +23,46 @@ func main() {
 	}
 
 	log.Println(resp)
+}
+
+
+// 读取终端输入
+func Input() map[string]string {
+	var err error
+	input := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("Please enter item id:")
+	item_id, err := input.ReadString('\n')
+	if err != nil {
+		log.Println("input item_id", err)
+	}
+
+	fmt.Printf("Please enter Count:")
+	itemnum, err := input.ReadString('\n')
+	if err != nil {
+		log.Println("input itemCount", err)
+	}
+
+	fmt.Printf("Please enter userId:")
+	userid, err := input.ReadString('\n')
+	if err != nil {
+		log.Println("input userid", err)
+	}
+
+	fmt.Printf("remark or not:")
+	descr, err := input.ReadString('\n')
+	if descr == "" {
+		descr = "nil"
+	}
+	if err != nil {
+		log.Println("description", err)
+	}
+
+	var InputMap = map[string]string{
+		"item_id": item_id,
+		"itemCount": itemnum,
+		"user_id": userid,
+		"descri": descr
+	}
+	return InputMap
 }
