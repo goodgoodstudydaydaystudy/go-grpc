a455aecc14edc905b6ea857c041468604714a0ad
Water <water@WaterdeiMac.local>
2020-01-10 14:37:29 +0800

    waite test

--- cmd/Client.go
+++ cmd/Client.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"goodgoodstudy.com/go-grpc/pkg/client"
+	pb "goodgoodstudy.com/go-grpc/pkg/pb"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -38,19 +40,25 @@ func Input() {
 	if err != nil {
 		log.Println("input item_id", err)
 	}
-	client.ConsumeClient{}.Pay()
+	id, _ := strconv.ParseInt(item_id, 10, 64)
+	client.ConsumeClient{}.Pay(&pb.ConsumeReq{ItemId:id})
+
 
 	fmt.Printf("Please enter Count:")
 	itemnum, err := input.ReadString('\n')
 	if err != nil {
 		log.Println("input itemCount", err)
 	}
+	num, _ := strconv.ParseInt(itemnum, 10, 64)
+	client.ConsumeClient{}.Pay(&pb.ConsumeReq{ItemNum:num})
 
 	fmt.Printf("Please enter userId:")
 	userid, err := input.ReadString('\n')
 	if err != nil {
 		log.Println("input userid", err)
 	}
+	constomerid, _ := strconv.ParseInt(userid, 10, 32)
+	client.ConsumeClient{}.Pay(&pb.ConsumeReq{UserId:int32(constomerid)})
 
 	fmt.Printf("remark or not:")
 	descr, err := input.ReadString('\n')
@@ -60,4 +68,5 @@ func Input() {
 	if err != nil {
 		log.Println("description", err)
 	}
+	client.ConsumeClient{}.Pay(&pb.ConsumeReq{Description:descr})
 }
