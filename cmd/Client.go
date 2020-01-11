f8cdabbd3683e0bb5ec17701b32ef2243608c3e6
orangetangerine <orangetangerineshen@gmail.com>
2020-01-12 00:40:44 +0800

    repair

--- cmd/Client.go
+++ cmd/Client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"goodgoodstudy.com/go-grpc/pkg/client"
 	pb "goodgoodstudy.com/go-grpc/pkg/pb"
@@ -18,21 +19,28 @@ func main() {
 		log.Println("new client:", err)
 	}
 
-	resp, err := consumeClient.Pay(nil) // TODO
+	ctx := context.Background()
+
+	resp, err := consumeClient.Pay(ctx, nil) // TODO
 	if err != nil {
 		log.Println("consume failed,", err)
 		return
 	}
 
-
-
 	log.Println(resp)
+
+	_ = consumeClient.Close()
 }
 
-
 // 读取终端输入
+// 这个函数没有意义
 func Input() {
-	var err error
+	ctx := context.Background()
+	consumeClient, err := client.NewConsumeClient()
+	if err != nil {
+		log.Println("NewConsumeClient", err)
+	}
+
 	input := bufio.NewReader(os.Stdin)
 
 	fmt.Printf("Please enter item id:")
@@ -41,8 +49,7 @@ func Input() {
 		log.Println("input item_id", err)
 	}
 	id, _ := strconv.ParseInt(item_id, 10, 64)
-	client.ConsumeClient{}.Pay(&pb.ConsumeReq{ItemId:id})
-
+	consumeClient.Pay(ctx, &pb.ConsumeReq{ItemId: id})
 
 	fmt.Printf("Please enter Count:")
 	itemnum, err := input.ReadString('\n')
@@ -50,7 +57,7 @@ func Input() {
 		log.Println("input itemCount", err)
 	}
 	num, _ := strconv.ParseInt(itemnum, 10, 64)
-	client.ConsumeClient{}.Pay(&pb.ConsumeReq{ItemNum:num})
+	consumeClient.Pay(ctx, &pb.ConsumeReq{ItemNum: num})
 
 	fmt.Printf("Please enter userId:")
 	userid, err := input.ReadString('\n')
@@ -58,7 +65,7 @@ func Input() {
 		log.Println("input userid", err)
 	}
 	constomerid, _ := strconv.ParseInt(userid, 10, 32)
-	client.ConsumeClient{}.Pay(&pb.ConsumeReq{UserId:int32(constomerid)})
+	consumeClient.Pay(ctx, &pb.ConsumeReq{UserId: int32(constomerid)})
 
 	fmt.Printf("remark or not:")
 	descr, err := input.ReadString('\n')
@@ -68,5 +75,5 @@ func Input() {
 	if err != nil {
 		log.Println("description", err)
 	}
-	client.ConsumeClient{}.Pay(&pb.ConsumeReq{Description:descr})
+	consumeClient.Pay(ctx, &pb.ConsumeReq{Description: descr})
 }
--- pkg/client/Client.go
+++ pkg/client/Client.go
@@ -12,7 +12,9 @@ const (
 )
 
 type ConsumeClient struct {
-	stub pb.ControlClient
+	conn *grpc.ClientConn // 用于关闭连接等
+
+	cli pb.ControlClient
 }
 
 func NewConsumeClient() (*ConsumeClient, error) {
@@ -20,19 +22,23 @@ func NewConsumeClient() (*ConsumeClient, error) {
 	if err != nil {
 		log.Printf("failed to listen: %v", err)
 	}
-	defer conn.Close()
 
-	client := &ConsumeClient{}
+	controlClient := pb.NewControlClient(conn)
+	return &ConsumeClient{
+		conn: conn,
+		cli:  controlClient,
+	}, nil
+}
 
-	client.stub = pb.NewControlClient(conn)
-	return client, nil
+func (c *ConsumeClient) Close() error {
+	return c.conn.Close()
 }
 
 // 发送 购买订单 数据
-func (c *ConsumeClient) Pay(req *pb.ConsumeReq) (resp *pb.ConsumeResp, err error) {
-	PayRep, err := c.stub.Pay(context.Background(), req)
+func (c *ConsumeClient) Pay(ctx context.Context, req *pb.ConsumeReq) (resp *pb.ConsumeResp, err error) {
+	resp, err = c.cli.Pay(ctx, req) // 注意这里没有用:=而已=, 因为函数声明里面返回值已经有名称了, 等于声明了变量
 	if err != nil {
-		log.Println("stub.Pay failed:", err)
+		log.Println("Pay failed:", err)
 	}
-	return PayRep, nil
+	return // 注意这里没有写返回哪个变量, 因为函数声明的返回值里面给了变量名字, 默认就是返回那些变量
 }
