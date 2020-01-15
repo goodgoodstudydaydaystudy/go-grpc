1d12fd17e2050090d300df8bb4cedc3ab0efde1f
Water <water@WaterdeiMac.local>
2020-01-16 01:08:45 +0800

    AccountServer

--- cmd/Server.go
+++ cmd/Server.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	pb "goodgoodstudy.com/go-grpc/pkg/pb"
-	rpb "goodgoodstudy.com/go-grpc/pkg/pb/Register"
-	"goodgoodstudy.com/go-grpc/pkg/server"
+	rpb "goodgoodstudy.com/go-grpc/pkg/pb/Account"
+	"goodgoodstudy.com/go-grpc/pkg/pb/Pay"
+	"goodgoodstudy.com/go-grpc/server"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
@@ -26,13 +26,12 @@ func main() {
 	s := grpc.NewServer()
 
 	// 注册ControlServer
-	pb.RegisterControlServer(s, &server.ControlServer{})
-	rpb.RegisterNewconstomerServer(s, &server.NewconstomerServer{})
+	Pay.RegisterControlServer(s, &server.ControlServer{})
+	rpb.RegisterAccountServer(s, &server.Server{})
 
 	// 注册反射服务
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to server: %v", err)
 	}
-	log.Printf("creat grpc server success")
 }
