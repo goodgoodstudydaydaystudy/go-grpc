4b4dbf37d322a31ce98557e36428f32d96dabb29
strongChicKen <mrwater93@163.com>
2020-01-24 00:41:09 +0800

    errorCode

--- pkg/server/account/account_server.go
+++ pkg/server/account/account_server.go
@@ -3,34 +3,14 @@ package account
 import (
 	"context"
 	rpb "goodgoodstudy.com/go-grpc/pkg/pb/account"
+	protocol "goodgoodstudy.com/go-grpc/pkg/procotol"
 	"goodgoodstudy.com/go-grpc/pkg/server/account/dao"
-
 	"log"
 	"math/rand"
 )
 
 type server struct {
 	db dao.AccountDao
-}
-
-// 服务器方法
-// 方法名要和rpc接口一致，否则client注册服务器会报错
-
-// 注册
-func (s *server) Register(ctx context.Context, req *rpb.RegisterReq) (*rpb.RegisterResp, error) {
-	// 返回给用户的注册id
-	userId := rand.Int31()
-	err := s.db.InsertInfo(userId, req.GetAccount(), req.GetPassword())
-	if err != nil {
-		log.Println("db.insert failed: ", err)
-		return &rpb.RegisterResp{Message: "register failed"}, err
-	}
-	return &rpb.RegisterResp{Message: "register success", UeserId: userId}, nil
-}
-
-// 登录
-func (s *server) Login(ctx context.Context, req *rpb.LoginReq) (*rpb.LoginResp, error) {
-	return &rpb.LoginResp{Message: "login success"}, nil
 }
 
 // server connDb
@@ -40,3 +20,25 @@ func NewAccountServer() (*server, error) {
 		db: db,
 	}, err
 }
+
+
+// 方法名要和rpc接口一致，否则client注册服务器会报错
+// server的注册功能
+// TODO 返回"已注册"的错误信息
+func (s *server) Register(ctx context.Context, req *rpb.RegisterReq) (*rpb.RegisterResp, error) {
+	// 返回给用户的注册id
+	userId := rand.Int31()
+	err := s.db.InsertInfo(userId, req.GetAccount(), req.GetPassword())
+	if err != nil {
+		log.Println("db.insert failed: ",err)
+		return &rpb.RegisterResp{Message: ""}, protocol.ToServerError(err)
+	}
+	return &rpb.RegisterResp{Message: "register success", UeserId: userId}, protocol.ToServerError(err)
+}
+
+// 登录功能
+func (s *server) Login(ctx context.Context, req *rpb.LoginReq) (*rpb.LoginResp, error) {
+	return &rpb.LoginResp{Message: "login success"}, nil
+}
+
+
